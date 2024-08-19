bc0199900fd0b44a3c54519d355ff0f832ce362f
alanfzf <alandavidg13@gmail.com>
2024-08-19 09:47:43 +0000

    feat(components): create first components

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.base')
 
 @section('main')
-    <h1>hola</h1>
+    <div class="flex w-full flex-col border-opacity-50">
+        <div class="divider">Law compliance</div>
+    </div>
+
+    <div class="flex flex-row flex-wrap gap-3 items-center justify-center">
+
+        @for ($i = 0; $i < 10; $i++)
+            <x-laws.card></x-laws.card>
+        @endfor
+
+    </div>
 @endsection
