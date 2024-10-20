0959088bf0cc736181362ee14a58b696948a1d28
alanfzf <alandavidg13@gmail.com>
2024-10-20 00:09:55 +0000

    fix(deploy): wrong docker compose file name

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker compose -f prod.docker.compose.yml pull
-docker compose -f prod.docker.compose.yml up -d
+docker compose -f prod.docker-compose.yml pull
+docker compose -f prod.docker-compose.yml up -d
 docker image prune -f
